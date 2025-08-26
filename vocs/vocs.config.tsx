9fa54ffc3d18797e84f7332a36b4d8f76a7eb350
Matthias Seitz <matthias.seitz@outlook.de>
2025-08-26 10:55:03 +0200

    chore: v1.0.26

--- vocs/vocs.config.tsx
+++ vocs/vocs.config.tsx
@@ -55,7 +55,7 @@ export default defineConfig({
       link: 'https://docs.rs/alloy/latest/alloy/',
     },
     { 
-      text: '1.0.24',
+      text: '1.0.26',
       items: [ 
         { 
           text: 'Changelog', 
