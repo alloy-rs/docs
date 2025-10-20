579bedb6a15b76d9f0b3cb1d331b7c058c73678a
Matthias Seitz <matthias.seitz@outlook.de>
2025-10-20 11:02:57 +0200

    chode: bump docs 1.0.41 (#158)

--- vocs/vocs.config.tsx
+++ vocs/vocs.config.tsx
@@ -55,7 +55,7 @@ export default defineConfig({
       link: 'https://docs.rs/alloy/latest/alloy/',
     },
     { 
-      text: '1.0.38',
+      text: '1.0.41',
       items: [ 
         { 
           text: 'Changelog', 
